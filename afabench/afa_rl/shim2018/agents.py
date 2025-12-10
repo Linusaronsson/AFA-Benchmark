fix: get_eval_metrics correctly returns sum of rewards averaged over episodes

afabench/afa_rl/shim2018/agents.py
@@ -55,19 +55,80 @@ class Shim2018ActionValueModule(nn.Module):
         feature_mask: FeatureMask,
         action_mask: Tensor,
     ) -> Tensor:
-        # We do not want to update the embedder weights using the Q-values, this is done separately in the training loop
-        # FIX:
+        # Flatten feature dimensions
         flat_masked_features = masked_features.flatten(
             start_dim=-self.n_feature_dims
         )
         flat_feature_mask = feature_mask.flatten(
             start_dim=-self.n_feature_dims
         )
+        # Flatten batch dimensions
+        flat_masked_features = flat_masked_features.flatten(end_dim=-2)
+        flat_feature_mask = flat_feature_mask.flatten(end_dim=-2)
+        # We do not want to update the embedder weights using the Q-values, this is done separately in the training loop
         with torch.no_grad():
             embedding = self.embedder(flat_masked_features, flat_feature_mask)
         qvalues = self.net(embedding)
+        # Unflatten batch dimensions
+        qvalues = qvalues.unflatten(
+            0, masked_features.shape[: -self.n_feature_dims]
+        )
 
-        # qvalues = self.net(torch.cat([masked_features, feature_mask], dim=-1))
+        # By setting the Q-values of invalid actions to -inf, we prevent them from being selected greedily.
+        qvalues[~action_mask] = float("-inf")
+        return qvalues
+
+
+@final
+class Shim2018DummyActionValueModule(nn.Module):
+    def __init__(
+        self,
+        in_size: int,
+        action_size: int,
+        num_cells: tuple[int, ...],
+        dropout: float,
+        n_feature_dims: int,
+    ):
+        super().__init__()
+        self.in_size = in_size
+        self.action_size = action_size
+        self.num_cells = num_cells
+        self.dropout = dropout
+        self.n_feature_dims = n_feature_dims
+
+        self.net = MLP(
+            in_features=self.in_size,
+            out_features=self.action_size,
+            num_cells=self.num_cells,
+            dropout=self.dropout,
+            activation_class=nn.ReLU,
+        )
+
+    @override
+    def forward(
+        self,
+        masked_features: MaskedFeatures,
+        feature_mask: FeatureMask,
+        action_mask: Tensor,
+    ) -> Tensor:
+        # Flatten feature dimensions
+        flat_masked_features = masked_features.flatten(
+            start_dim=-self.n_feature_dims
+        )
+        flat_feature_mask = feature_mask.flatten(
+            start_dim=-self.n_feature_dims
+        )
+        # Flatten batch dimensions
+        flat_masked_features = flat_masked_features.flatten(end_dim=-2)
+        flat_feature_mask = flat_feature_mask.flatten(end_dim=-2)
+        qvalues = self.net(
+            torch.cat([flat_masked_features, flat_feature_mask], dim=-1)
+        )
+        # Unflatten batch dimensions
+        qvalues = qvalues.unflatten(
+            0, masked_features.shape[: -self.n_feature_dims]
+        )
+
         # By setting the Q-values of invalid actions to -inf, we prevent them from being selected greedily.
         qvalues[~action_mask] = float("-inf")
         return qvalues
@@ -96,10 +157,8 @@ class Shim2018Agent(Agent):
         self.module_device = module_device
         self.n_feature_dims = n_feature_dims
 
-        self.action_value_module = Shim2018ActionValueModule(
-            embedder=self.embedder,
-            embedding_size=self.embedding_size,  # FIX:
-            # embedding_size=12,
+        self.action_value_module = Shim2018DummyActionValueModule(
+            in_size=40,
             action_size=self.action_spec.n,  # pyright: ignore[reportAttributeAccessIssue]
             num_cells=tuple(self.cfg.action_value_num_cells),
             dropout=self.cfg.action_value_dropout,
@@ -190,9 +249,11 @@ class Shim2018Agent(Agent):
 
     @override
     def process_batch(self, td: TensorDictBase) -> dict[str, Any]:
-        assert td.batch_size == torch.Size((self.batch_size,)), (
-            "Batch size mismatch"
-        )
+        # assert td.batch_size == torch.Size((self.batch_size,)), (
+        #     "Batch size mismatch"
+        # )
+
+        # td = tds.flatten(start_dim=0, end_dim=1)
 
         # Initialize total loss dictionary
         total_loss_dict = {"loss": 0.0}

afabench/afa_rl/shim2018/reward.py
@@ -1,7 +1,6 @@
 import torch
 from jaxtyping import Bool
 from torch import Tensor
-from torch.nn import functional as F
 
 from afabench.afa_rl.custom_types import (
     AFAReward,
@@ -19,9 +18,9 @@ from afabench.common.custom_types import (
 
 
 def get_shim2018_reward_fn(
-    pretrained_model: LitShim2018EmbedderClassifier,
-    weights: Tensor,
-    acquisition_costs: torch.Tensor,
+    _pretrained_model: LitShim2018EmbedderClassifier,
+    _weights: Tensor,
+    _acquisition_costs: torch.Tensor,
 ) -> AFARewardFn:
     """
     Return the reward function for shim2018.
@@ -34,30 +33,41 @@ def get_shim2018_reward_fn(
         _masked_features: MaskedFeatures,
         _feature_mask: FeatureMask,
         selection_mask: SelectionMask,
-        new_masked_features: MaskedFeatures,
-        new_feature_mask: FeatureMask,
+        _new_masked_features: MaskedFeatures,
+        _new_feature_mask: FeatureMask,
         new_selection_mask: SelectionMask,
         _afa_selection: AFASelection,
         features: Features,  # noqa: ARG001
-        label: Label,
-        done: Bool[Tensor, "*batch 1"],
+        _label: Label,
+        _done: Bool[Tensor, "*batch 1"],
     ) -> AFAReward:
         # Acquisition cost per selection
+        # newly_performed_selections = (new_selection_mask & ~selection_mask).to(
+        #     torch.float32
+        # )
+        # reward = -(newly_performed_selections * acquisition_costs).sum(dim=-1)
+
+        # done_mask = done.squeeze(-1)
+
+        # if done_mask.any():
+        #     _, logits = pretrained_model(
+        #         new_masked_features[done_mask], new_feature_mask[done_mask]
+        #     )
+        #     reward[done_mask] = -F.cross_entropy(
+        #         logits, label[done_mask], weight=weights
+        #     )
+
+        # return reward.unsqueeze(-1)
+        # DEBUG START
+        # Give a fixed reward every time selections 5-10 are made, just for debugging
         newly_performed_selections = (new_selection_mask & ~selection_mask).to(
             torch.float32
         )
-        reward = -(newly_performed_selections * acquisition_costs).sum(dim=-1)
+        reward = newly_performed_selections[..., 5:11].sum(
+            dim=-1, keepdim=True
+        )
+        return reward
 
-        done_mask = done.squeeze(-1)
-
-        if done_mask.any():
-            _, logits = pretrained_model(
-                new_masked_features[done_mask], new_feature_mask[done_mask]
-            )
-            reward[done_mask] = -F.cross_entropy(
-                logits, label[done_mask], weight=weights
-            )
-
-        return reward.unsqueeze(-1)
+        # DEBUG END
 
     return f

scripts/train/shim2018.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any, cast
 
 import hydra
 import torch
+import wandb
 from omegaconf.omegaconf import OmegaConf
 from rl_helpers import dict_with_prefix
 from torch import optim
@@ -147,9 +148,9 @@ def main(cfg: Shim2018TrainConfig) -> None:  # noqa: PLR0915
         0 if cfg.soft_budget_param is None else cfg.soft_budget_param
     )
     reward_fn = get_shim2018_reward_fn(
-        pretrained_model=pretrained_model,
-        weights=class_weights,
-        acquisition_costs=cost_per_selection
+        _pretrained_model=pretrained_model,
+        _weights=class_weights,
+        _acquisition_costs=cost_per_selection
         * torch.ones(
             (n_selections,),
             device=device,
@@ -233,13 +234,12 @@ def main(cfg: Shim2018TrainConfig) -> None:  # noqa: PLR0915
     # Training loop
     log.info(f"Starting training loop for {cfg.n_batches} batches")
     try:
-        for batch_idx, tds in tqdm(
+        for batch_idx, td in tqdm(
             enumerate(collector), total=cfg.n_batches, desc="Training agent..."
         ):
             collector.update_policy_weights_()
 
             # Collapse agent and batch dimensions
-            td = tds.flatten(start_dim=0, end_dim=1)
             loss_info = agent.process_batch(td)
 
             # Train classifier and embedder jointly if we have reached the correct batch
@@ -291,6 +291,10 @@ def main(cfg: Shim2018TrainConfig) -> None:  # noqa: PLR0915
                         .mean()
                         .cpu()
                         .item(),
+                        # Action histogram, comment if too expensive
+                        "actions": wandb.Histogram(
+                            td["action"].flatten().cpu().numpy()
+                        ),
                         "batch_idx": batch_idx,
                     }
                     | {"class_loss": class_loss_next.mean().cpu().item()},
@@ -317,9 +321,9 @@ def main(cfg: Shim2018TrainConfig) -> None:  # noqa: PLR0915
                     ]
                     # Reset the action spec of the agent to the train env action spec
                     agent.egreedy_tdmodule._spec = train_env.action_spec  # noqa: SLF001
-                metrics_eval = get_eval_metrics(
-                    td_evals, Shim2018AFAPredictFn(pretrained_model)
-                )
+                    metrics_eval = get_eval_metrics(
+                        td_evals, Shim2018AFAPredictFn(pretrained_model)
+                    )
                 log_fn(
                     dict_with_prefix(
                         "eval/",

test/afa_rl/shim2018/test_shim2018.py
@@ -367,9 +367,9 @@ class TestShim2018Environment:
         acquisition_costs = 0.1 * torch.ones(n_selections, device=device)
 
         reward_fn = get_shim2018_reward_fn(
-            pretrained_model=lit_model,
-            weights=class_weights,
-            acquisition_costs=acquisition_costs,
+            lit_model,
+            class_weights,
+            acquisition_costs,
         )
 
         # Create environment
@@ -466,9 +466,9 @@ class TestShim2018Environment:
         acquisition_costs = 0.1 * torch.ones(n_selections, device=device)
 
         reward_fn = get_shim2018_reward_fn(
-            pretrained_model=lit_model,
-            weights=class_weights,
-            acquisition_costs=acquisition_costs,
+            lit_model,
+            class_weights,
+            acquisition_costs,
         )
 
         env = AFAEnv(
@@ -558,9 +558,9 @@ class TestShim2018Integration:
         acquisition_costs = 0.1 * torch.ones(n_selections, device=device)
 
         reward_fn = get_shim2018_reward_fn(
-            pretrained_model=lit_model,
-            weights=class_weights,
-            acquisition_costs=acquisition_costs,
+            lit_model,
+            class_weights,
+            acquisition_costs,
         )
 
         env = AFAEnv(