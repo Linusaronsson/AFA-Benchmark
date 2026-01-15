28f1329ad8685a3c670425d7a4fc2f1879ce6cb8
Valter Schütz <valterschutz@proton.me>
2026-01-15 17:28:36 +0100

    feat: script for plotting evaluation time

---
+++ scripts/plotting/plot_eval_time.R
@@ -0,0 +1,32 @@
+options(device = "null")
+
+suppressPackageStartupMessages({
+  library(ggplot2)
+  library(dplyr)
+  library(readr)
+  library(yardstick)
+  library(stringr)
+  library(ggbeeswarm)
+})
+
+read_csv_safe <- function(path) {
+  df <- read_csv(path, col_types = list(
+    afa_method = col_factor(),
+    dataset = col_factor(),
+    eval_time = col_number()
+  ))
+  df
+}
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 2) {
+  df <- read_csv_safe(args[1])
+  output_path <- args[2]
+} else {
+  stop("Usage: Rscript plot.R eval_training_times.csv output_folder")
+}
+
+plot <- ggplot(df, aes(x = afa_method, y = eval_time, color = dataset)) +
+  geom_beeswarm()
+
+ggsave(str_c(output_path, "/beeswarm_plot.svg"), plot, create.dir = TRUE)
