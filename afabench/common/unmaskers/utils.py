409b20244a46c9d92a7378530744e5040ded8caa
Valter Schütz <valterschutz@proton.me>
2026-02-06 13:44:48 +0100

    fix: make AFAContextUnmasker loadable from config

--- afabench/common/unmaskers/utils.py
+++ afabench/common/unmaskers/utils.py
@@ -1,6 +1,7 @@
 from afabench.common.config_classes import UnmaskerConfig
 from afabench.common.custom_types import AFAUnmasker
 from afabench.common.registry import get_class
+from afabench.common.unmaskers.afa_context_unmasker import AFAContextUnmasker
 from afabench.common.unmaskers.direct_unmasker import DirectUnmasker
 from afabench.common.unmaskers.image_patch_unmasker import ImagePatchUnmasker
 
@@ -19,6 +20,11 @@ def get_afa_unmasker_from_config(
     if unmasker_config.class_name == "ImagePatchUnmasker":
         cls = get_class(unmasker_config.class_name)
         assert cls is ImagePatchUnmasker
+        return cls(**unmasker_config.kwargs)
+
+    if unmasker_config.class_name == "AFAContextUnmasker":
+        cls = get_class(unmasker_config.class_name)
+        assert cls is AFAContextUnmasker
         return cls(**unmasker_config.kwargs)
 
     msg = f"Unknown unmasker: {unmasker_config.class_name}"
